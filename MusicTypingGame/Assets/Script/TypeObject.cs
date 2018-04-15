db241290e19508c30b3383732ca9e8717af2ddaf
Katsumata_Atsushi <atsuatsu1130@gmail.com>
2018-04-16 00:49:38 +0900

    タイピングゲームの土台完成

---
+++ MusicTypingGame/Assets/Script/TypeObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeObject : MonoBehaviour {
+
+    public TextMesh stringTextMesh;
+    public TextMesh alphabetTextMesh;
+    TypingSystem ts;
+    public string String;
+
+    void Start()
+    {
+        
+        ts = new TypingSystem();
+        ts.SetInputString(String);
+        UpdateText();
+    }
+
+    void Update()
+    {
+        string[] keys = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                          "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                          "w", "x", "y", "z", "-", };
+        foreach (string key in keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (ts.InputKey(key) == 1)
+                {
+                    UpdateText();
+                }
+                break;
+            }
+        }
+    }
+
+    void UpdateText()
+    {
+        stringTextMesh.text = "<color=red>" + ts.GetInputedString() + "</color>" + ts.GetRestString();
+        alphabetTextMesh.text = "<color=red>" + ts.GetInputedKey() + "</color>" + ts.GetRestKey();
+    }
+}
